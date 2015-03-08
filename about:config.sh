557c427ef8fad7a72d00b2f978181b6599497339
Shyam Nath <Sharminator@users.noreply.github.com>
2015-03-08 19:11:00 +1100

    Create about:config.sh

---
+++ about:config.sh
@@ -0,0 +1,7 @@
+# http://kb.mozillazine.org/User.js_file#Removing_user.js_entries
+echo "\
+// When Firefox starts: Show my windows and tabs from last time
+user_pref("browser.startup.page", 3);
+// Use autoscrolling (middle click and drag to navigate the page)
+user_pref("general.autoScroll", true);
+" > ~/.mozilla/firefox/$(ls | grep *.default)/user.js;# The profile directory? What if (profiles>1)?
