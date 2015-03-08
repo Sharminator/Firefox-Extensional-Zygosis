07e78ddf91004250b264d3551a6bb524dcdfe915
Shyam Nath <Sharminator@users.noreply.github.com>
2015-03-08 19:17:18 +1100

    Update about:config.sh

--- about:config.sh
+++ about:config.sh
@@ -3,5 +3,5 @@ echo "\
 // When Firefox starts: Show my windows and tabs from last time
 user_pref("browser.startup.page", 3);
 // Use autoscrolling (middle click and drag to navigate the page)
-user_pref("general.autoScroll", true);
-" > ~/.mozilla/firefox/$(ls | grep *.default)/user.js;# The profile directory? What if (profiles>1)?
+user_pref("general.autoScroll", true);\
+" > ~/.mozilla/firefox/$(ls ~/.mozilla/firefox/ | grep .default)/user.js; # The profile directory? What if (profiles>1)?
