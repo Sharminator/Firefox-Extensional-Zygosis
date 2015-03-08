99a2ede8fc85e1e46ebcda18f018088ce6acf128
Shyam Nath <Sharminator@users.noreply.github.com>
2015-03-08 18:30:18 +1100

    Create FEZ.sh

---
+++ FEZ.sh
@@ -0,0 +1,16 @@
+#Need to find a better way of doing this...
+
+firefox\
+    https://addons.mozilla.org/firefox/downloads/latest/tab-for-a-cause/\
+    https://addons.mozilla.org/firefox/downloads/latest/ecosia-the-green-search/\
+    https://addons.mozilla.org/firefox/downloads/latest/ecolink/\
+    https://addons.mozilla.org/firefox/downloads/latest/calomel-ssl-validation/\
+    https://addons.mozilla.org/firefox/downloads/latest/wot-safe-browsing-tool/\
+    https://addons.mozilla.org/firefox/downloads/latest/flagfox/\
+    https://addons.mozilla.org/firefox/downloads/latest/greasemonkey/\
+    https://addons.mozilla.org/firefox/downloads/latest/stylish/\
+    https://addons.mozilla.org/firefox/downloads/latest/ublock/\
+    https://addons.mozilla.org/firefox/downloads/latest/cleantube/\
+    https://addons.mozilla.org/firefox/downloads/latest/bloody-vikings/\
+    https://addons.mozilla.org/firefox/downloads/latest/imacros-for-firefox/\
+;
