925b0a674de092c1c25d9324399b61f72c2d3a55
Shyam Nath <Sharminator@users.noreply.github.com>
2015-03-13 03:30:47 +1100

    Update and rename about:config.sh to netinst.sh

--- netinst.sh
+++ netinst.sh
@@ -1,3 +1,7 @@
+# Java
+sudo apt-get install openjdk-7-jdk openjdk-7-jre icedtea-netx --no-install-recommends
+
+# about:config
 # http://kb.mozillazine.org/User.js_file#Removing_user.js_entries
 echo '
 // When Firefox starts: Show my windows and tabs from last time
