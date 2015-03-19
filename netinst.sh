dc7c87f86ef8062bb5115a6633282a694a304c08
Shyam Nath <Sharminator@users.noreply.github.com>
2015-03-20 03:18:44 +1100

    Update netinst.sh

--- netinst.sh
+++ netinst.sh
@@ -6,7 +6,7 @@ sudo apt-get install\
     openjdk-7-jdk openjdk-7-jre icedtea-netx\ # Java
     iceweasel\ # Web Browser & File Viewer
     gimp inkscape blender\ # Graphical Editors (Rasta, Vector, 3D)
-    --no-install-recommends\
+    --no-install-recommends --assume-yes\
 ;
 
 ######## Firefox || Iceweasel || GNU IceCat ########
